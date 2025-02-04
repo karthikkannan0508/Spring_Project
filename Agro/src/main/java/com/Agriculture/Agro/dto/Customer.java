8f425c6a781056d372071bb6c96165bbf1de8d4f
Karthik M K <152353940+karthikkannan0508@users.noreply.github.com>
2025-02-04 19:58:56 +0530

    update

--- Agro/src/main/java/com/Agriculture/Agro/dto/Customer.java
+++ Agro/src/main/java/com/Agriculture/Agro/dto/Customer.java
@@ -1,5 +1,43 @@
 package com.Agriculture.Agro.dto;
 
 public class Customer {
+	private int cid;
+	private String firstName;
+	private String lastName;
+	private long cPhone;
+	
+	//Getters and Setters
+	
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getcPhone() {
+		return cPhone;
+	}
+	public void setcPhone(long cPhone) {
+		this.cPhone = cPhone;
+	}
+	@Override
+	public String toString() {
+		return "Customer [cid=" + cid + ", firstName=" + firstName + ", lastName=" + lastName + ", cPhone=" + cPhone
+				+ "]";
+	}
+	
+	
 
 }
--- Agro/src/main/java/com/Agriculture/Agro/dto/Farmer.java
+++ Agro/src/main/java/com/Agriculture/Agro/dto/Farmer.java
@@ -1,5 +1,43 @@
 package com.Agriculture.Agro.dto;
 
 public class Farmer {
-
+	private int fid;
+	private String firstName;
+	private String lastName;
+	private long fPhone;
+	
+	//Getters and Setters
+	
+	public int getFid() {
+		return fid;
+	}
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getfPhone() {
+		return fPhone;
+	}
+	public void setfPhone(long fPhone) {
+		this.fPhone = fPhone;
+	}
+	@Override
+	public String toString() {
+		return "Farmer [fid=" + fid + ", firstName=" + firstName + ", lastName=" + lastName + ", fPhone=" + fPhone
+				+ "]";
+	}
+	
+	
+	
 }
